e2a84ee7f3c6b458ae65a2b7bd0043651f46cbd5
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2020-01-29 14:53:42 -0500

    new repo

---
+++ build/precache-manifest.67d3b765cfd47b8d1313f311731a5689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af58006c32f26de0351d6879cdbb1d77",
+    "url": "/Pomodoro-React-App/index.html"
+  },
+  {
+    "revision": "d6b0415ca4ff12264c4d",
+    "url": "/Pomodoro-React-App/static/css/main.b22dcfc8.chunk.css"
+  },
+  {
+    "revision": "b39a8377a6f387468f32",
+    "url": "/Pomodoro-React-App/static/js/2.cc11cb81.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Pomodoro-React-App/static/js/2.cc11cb81.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6b0415ca4ff12264c4d",
+    "url": "/Pomodoro-React-App/static/js/main.f9b5879a.chunk.js"
+  },
+  {
+    "revision": "7917d4cb632f8765bfce",
+    "url": "/Pomodoro-React-App/static/js/runtime-main.55ba3e3d.js"
+  }
+]);
